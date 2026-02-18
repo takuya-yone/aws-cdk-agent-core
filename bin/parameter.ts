5af0836742e3fbcccd03025c8d465d3b9698a11a
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2026-02-18 23:23:13 +0900

    fix: increase API Gateway timeout to 300 seconds

--- bin/parameter.ts
+++ bin/parameter.ts
@@ -47,6 +47,6 @@ export const defaultStackParameters: StackParameters = {
   },
   apiGwConfig: {
     stageName: "v1",
-    timeoutSeconds: Duration.seconds(90),
+    timeoutSeconds: Duration.seconds(300),
   },
 }
