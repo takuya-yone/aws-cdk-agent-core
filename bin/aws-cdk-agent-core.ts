55bd95109b79c5551fd6a5ff805774e5409c8681
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2026-02-20 00:53:52 +0900

    fix: reorder imports and comment out unused AgentCoreStack instantiation in aws-cdk-agent-core.ts

--- bin/aws-cdk-agent-core.ts
+++ bin/aws-cdk-agent-core.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib/core"
-// import { PipelineStack } from "../lib/pipeline-stack"
-import { AgentCoreStack } from "../lib/stack/agent-core-stack"
+import { PipelineStack } from "../lib/pipeline-stack"
+// import { AgentCoreStack } from "../lib/stack/agent-core-stack"
 import { defaultStackParameters } from "./parameter"
 
 const app = new cdk.App()
 
-// const _pipelineStack = new PipelineStack(
-//   app,
-//   "PipelineStack",
-//   defaultStackParameters,
-// )
-
-const _agentCoreStack = new AgentCoreStack(
+const _pipelineStack = new PipelineStack(
   app,
-  "AgentCoreStack",
+  "PipelineStack",
   defaultStackParameters,
 )
+
+// const _agentCoreStack = new AgentCoreStack(
+//   app,
+//   "AgentCoreStack",
+//   defaultStackParameters,
+// )
