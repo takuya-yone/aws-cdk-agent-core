b05dca1f2e8d0aff07309051c3da9919a6412aa4
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2026-02-21 02:46:17 +0900

    refactor(stack): rename variables for consistency and clarity

--- lib/pipeline-app-stage.ts
+++ lib/pipeline-app-stage.ts
@@ -7,14 +7,19 @@ export class StackStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props)
 
-    const _agentCoreStack = new AgentCoreStack(
+    const agentCoreStack = new AgentCoreStack(
       this,
       "AgentCoreStack",
       defaultStackParameters,
     )
 
-    const _sampleStack1 = new SampleStack(this, "SampleStack1")
-    const _sampleStack2 = new SampleStack(this, "SampleStack2")
-    const _sampleStack3 = new SampleStack(this, "SampleStack3")
+    const sampleStack1 = new SampleStack(this, "SampleStack1")
+    const sampleStack2 = new SampleStack(this, "SampleStack2")
+    const sampleStack3 = new SampleStack(this, "SampleStack3")
+
+    cdk.Tags.of(agentCoreStack).add("Project", "AwsCdkAgentCore")
+    cdk.Tags.of(sampleStack1).add("Project", "AwsCdkAgentCore")
+    cdk.Tags.of(sampleStack2).add("Project", "AwsCdkAgentCore")
+    cdk.Tags.of(sampleStack3).add("Project", "AwsCdkAgentCore")
   }
 }
